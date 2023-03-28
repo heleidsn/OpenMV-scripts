0f4bbb93892619b6a72d237a5dfbcaf5145b221e
Lei He <598269931@qq.com>
2023-03-28 12:12:41 +0800

    更新图像差分测试

--- 4_others/image_difference_test.py
+++ 4_others/image_difference_test.py
@@ -1,25 +1,25 @@
 # Untitled - By: helei - 周三 3月 22 2023
 # image difference test
+# 0328-实现图像差分，最大能处理QVGA分辨率灰度图像 46fps
 
 import sensor, image, time
 
 sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.set_framesize(sensor.QQVGA)
+sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 200)
 
 clock = time.clock()
 
 img_last = sensor.snapshot()
 
-# 设置额外的frame buffer
-extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.GRAYSCALE)
-extra_fb.replace(sensor.snapshot())
-
 while(True):
     clock.tick()
-    img = sensor.snapshot()
-    img = img.difference(extra_fb)
 
-    extra_fb.replace(sensor.snapshot())
+    img = sensor.snapshot()  # 获取图像
+    img_old = img.copy()     # 保留获取的图像
+    diff = img.difference(img_last)  # 计算差分
+
+    img_last = img_old       # 替换last
+
     print(clock.fps())
