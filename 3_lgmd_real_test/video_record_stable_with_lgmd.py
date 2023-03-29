2c405f3b066c84a9648aef7f8d0b0e18010c1d1e
He Lei <598269931@qq.com>
2023-03-29 14:37:00 +0800

    Update video_record_stable_with_lgmd.py

--- 3_lgmd_real_test/video_record_stable_with_lgmd.py
+++ 3_lgmd_real_test/video_record_stable_with_lgmd.py
@@ -1,18 +1,50 @@
-# Video_stable_v2
-# 使用rotation_correction对视频进行稳定
+# 最终避障版本
+# 使用rotation_correction对视频进行稳定，应该是最终用于实验的版本了
 # 2022-10-12
 # Lei He
-# 2023-01-30 上机测试
-# 2023-03-18 新校区测试 原始openmv损坏 无法检测到相机 更换新的openmv
-# 2023-03-21 能够同时记录稳定前和稳定后的视频 发现可以在不同阶段添加记录即可，不需要增加framebuffer
-# 2023-03-22 搞定兴趣区域，能够实现增稳和非增稳的切换，使用RC8, 调整了录制文件命名规则
-# 2023-03-22 增加LGMD相关内容
-# 2023-03-28 尝试添加LGMD所有内容 不做LGMD计算的情况下QVGA-210*100 gray输入，15 FPS 决定不做LGMD 直接用moment的图像 实现了图像分割
-# 发现串口无法通信，将线重新做了一遍，可以了
+
+'''
+Change logs:
+软件部分：
+2023-01-30 上机测试
+2023-03-18 新校区测试 原始openmv损坏 无法检测到相机 更换新的openmv
+2023-03-21 能够同时记录稳定前和稳定后的视频 发现可以在不同阶段添加记录即可，不需要增加framebuffer
+2023-03-22 搞定兴趣区域，能够实现增稳和非增稳的切换，使用RC8, 调整了录制文件命名规则
+2023-03-22 增加LGMD相关内容
+2023-03-28 尝试添加LGMD所有内容 不做LGMD计算的情况下QVGA-210*100 gray输入，15 FPS 决定不做LGMD 直接用moment的图像 实现了图像分割
+2023-03-29 新校区实验
+
+硬件部分：
+    发现串口无法通信，将线重新做了一遍，可以了
+'''
 
 import sensor, image, time, mjpeg, pyb, gc, struct, math
 from pyb import Pin, Timer, LED, RTC, ExtInt
 from pyb import UART
+from ulab import numpy as np
+
+# 计算yaw error
+def get_yaw_error(goal_pose, pose_and_yaw):
+    '''
+    根据目标点位置、当前位置得到相对航迹角
+    再根据航迹角和当前yaw得到yaw_err
+    '''
+    y_err = goal_pose[1] - pose_and_yaw[1]
+    x_err = goal_pose[0] - pose_and_yaw[0]
+    yaw_sp = math.atan2(y_err, x_err)
+
+    yaw_error = yaw_sp - pose_and_yaw[3]
+
+    # print('yaw_sp: ', yaw_sp* 57.3, 'yaw: ', pose_and_yaw[3] * 57.3, 'yaw_err: ', yaw_error * 57.3)
+
+    if yaw_error > math.pi:
+        yaw_error -= 2*math.pi
+    elif yaw_error < -math.pi:
+        yaw_error += 2*math.pi
+
+    return yaw_error
+
+
 
 # LGMD相关代码
 def get_moment_image(img_curr):
@@ -59,6 +91,46 @@ def get_split_out(img_moment):
 packet_sequence = 0
 MAV_system_id = 1
 MAV_component_id = 0x54
+MAVLINK_MSG_ID = 254  # DEBUG ( #254 )
+
+MAVLINK_MESSAGE_CRCS = [50, 124, 137, 0, 237, 217, 104, 119, 117, 0, 0, 89, 0, 0, 0, 0, 0, 0, 0, 137, 214, 159, 220, 168, 24, 23, 170, 144, 67, 115, 39, 246, 185, 104, 237, 244, 222, 212, 9, 254, 230, 28, 28, 132, 221, 232, 11, 153, 41, 39, 78, 196, 132, 0, 15, 3, 0, 0, 0, 0, 0, 167, 183, 119, 191, 118, 148, 21, 0, 243, 124, 0, 0, 38, 20, 158, 152, 143, 0, 0, 14, 106, 49, 22, 143, 140, 5, 150, 0, 231, 183, 63, 54, 47, 0, 0, 0, 0, 0, 0, 175, 102, 158, 208, 56, 93, 138, 108, 32, 185, 84, 34, 174, 124, 237, 4, 76, 128, 56, 116, 134, 237, 203, 250, 87, 203, 220, 25, 226, 46, 29, 223, 85, 6, 229, 203, 1, 195, 109, 168, 181, 47, 72, 131, 127, 0, 103, 154, 178, 200, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 189, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 208, 0, 0, 0, 0, 163, 105, 151, 35, 150, 179, 0, 0, 0, 0, 0, 90, 104, 85, 95, 130, 184, 81, 8, 204, 49, 170, 44, 83, 46, 0]
+
+def checksum(data, extra): # https://github.com/mavlink/c_library_v1/blob/master/checksum.h
+    output = 0xFFFF
+    for i in range(len(data)):
+        tmp = data[i] ^ (output & 0xFF)
+        tmp = (tmp ^ (tmp << 4)) & 0xFF
+        output = ((output >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)) & 0xFFFF
+    tmp = extra ^ (output & 0xFF)
+    tmp = (tmp ^ (tmp << 4)) & 0xFF
+    output = ((output >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)) & 0xFFFF
+    return output
+
+def send_debug_value(roll_cmd):
+    global packet_sequence
+    temp = struct.pack("<lfb",
+                       0,  # time_boot_ms uint32_t
+                       roll_cmd,              # value
+                       1                      # uint8_t ind
+                       )
+
+    # out = struct.unpack("<l10sf", temp)
+    # print(out, len(out))
+
+    temp = struct.pack("<bbbbb9s",
+                       9,                     # len
+                       packet_sequence & 0xFF, # seq
+                       MAV_system_id,          # sid
+                       MAV_component_id,       # cid
+                       MAVLINK_MSG_ID,         # mid
+                       temp)                   # data
+    temp = struct.pack("<b14sh",
+                       0xFE,                   # head
+                       temp,                   # data
+                       checksum(temp, MAVLINK_MESSAGE_CRCS[MAVLINK_MSG_ID]))
+    packet_sequence += 1
+    uart.write(temp)
+    time.sleep_ms(10)
 
 def get_attitude_force(is_sim=False):
     # 强制更新姿态信息
@@ -134,10 +206,16 @@ record_index_file = 'record_name.txt'       # 录制文件命名记录
 
 video_folder = 'video_record_stable_03_28_lgmd'  # 设置video记录地址
 
+control_weight = np.array([1, 2, 3, -2, -1, 3])  # 用于控制的权重，前五位为LGMD输出，后一位为yaw偏角
+roll_cmd_max = 40
+goal_pose = [160, 40]
+
 uart = UART(3, 3000000)
 
 flag_record_image = False
 flag_record_save = False
+flag_is_debug = False          # 是否为debug模式，即不需要飞控数据
+flag_use_stable = False
 
 rc_in = [0, 0]                    # rc_in分别对应通道7和通道8
 rc_in_last = [0, 0]               # 通道7用来控制是否录像 通道8用来控制录像准类
@@ -147,7 +225,7 @@ now = time.ticks_ms()
 
 step = 1 # used for update RC input
 
-flag_use_stable = False
+
 
 X_OFFSET = 0
 Y_OFFSET = 0
@@ -159,6 +237,7 @@ FOV_WINDOW = 80
 # 获取当前图像大小
 img_curr = sensor.snapshot()
 img_size = [img_curr.height(), img_curr.width()]  # 480 640
+print(img_curr.height(), img_curr.width())
 
 # 设置裁切大小
 w_crop = 210
@@ -183,12 +262,11 @@ while(True):
     step = step + 1
 
     # ------------------更新姿态和遥控信息---------------------------------
-    # print('wait for attitude')
-    attitude_rad = get_attitude_force(is_sim=True)  # 单独force更新能到80HZ 带上获取图像46Hz 带上
+    attitude_rad = get_attitude_force(is_sim=flag_is_debug)  # 单独force更新能到80HZ 带上获取图像46Hz 带上
 
     if step >= 10:
         # 每10步或者50步强制更新一下RC，将主要时间让给姿态更新
-        get_rc_force(is_sim=True)
+        get_rc_force(is_sim=flag_is_debug)
         step = 1
 
     # ------------------------判断是否进行录制---------------------------
@@ -220,13 +298,13 @@ while(True):
                 print('file_name: ', new_name)
 
             # 创建录制文件
-            video_path = video_folder + '/{}_stable.mjpeg'.format(new_name)
+            video_path_stable = video_folder + '/{}_stable.mjpeg'.format(new_name)
             video_path_origin = video_folder + '/{}_origin.mjpeg'.format(new_name)
-            video_path_crop = video_folder + '/{}_crop.mjpeg'.format(new_name)
+            video_path_moment = video_folder + '/{}_moment.mjpeg'.format(new_name)
 
-            m_origin = mjpeg.Mjpeg(video_path_origin)
-            m = mjpeg.Mjpeg(video_path)
-            m_crop = mjpeg.Mjpeg(video_path_crop, width=w_crop, height=h_crop)
+            m_origin = mjpeg.Mjpeg(video_path_origin, width=img_size[1], height=img_size[0])    # 原始视频
+            m_stable = mjpeg.Mjpeg(video_path_stable, width=w_crop, height=h_crop)              # 稳定后视频
+            m_moment = mjpeg.Mjpeg(video_path_moment, width=w_crop, height=h_crop)              # moment视频
 
             flag_record_image = True
             red_led.off()
@@ -268,90 +346,103 @@ while(True):
         y_center = int((img_size[0] - img_stable_h))
 
     if flag_use_stable:
+        '''
+        边处理边存储图像
+        由于OpenMV堆栈大小限制，无法对图像进行拷贝等，只能一边处理一边记录
+        '''
+
+        # 1 - 获取当前传感器图像
+        img_curr = sensor.snapshot()
+        # if flag_record_image:
+            # m_origin.add_frame(img_curr, quality=100)  # 存储原始图像
+
+        # 2 - 图像校正和消抖
+        img_curr = img_curr.lens_corr(2.2, 1)  # 镜头校正 原始值2.3 1
+        img_curr = img_curr.rotation_corr(x_rotation = pitch - pitch_offset_deg,
+                                           y_rotation = 0,
+                                           z_rotation = roll,
+                                           x_translation = X_OFFSET,
+                                           y_translation = -y_diff,
+                                           zoom = ZOOM_AMOUNT,
+                                           fov = FOV_WINDOW)
+
+        # 3 - 裁切 得到稳定后图像 320*240 220*100 roi=(x, y, w, h)
+        img_crop = img_curr.crop(roi=(x_crop, y_crop, w_crop, h_crop))
         if flag_record_image:
-            '''
-            边处理边存储图像
-            由于OpenMV堆栈大小限制，无法对图像进行拷贝等，只能一边处理一边记录
-            '''
+            m_stable.add_frame(img_crop, quality=100)   # 存储稳定后图像
 
-            # 1 - 获取当前传感器图像
-            img_curr = sensor.snapshot()
-            m_origin.add_frame(img_curr, quality=100)  # 存储原始图像
+        # 4 - 计算图像的moment
+        img_moment = get_moment_image(img_crop)
+        img_crop.replace(img_moment)  # 显示img_moment
 
-            # 2- 图像校正和消抖
-            img_curr = img_curr.lens_corr(2.2, 1)  # 镜头校正 原始值2.3 1
-            img_curr = img_curr.rotation_corr(x_rotation = pitch - pitch_offset_deg,
-                                               y_rotation = 0,
-                                               z_rotation = roll,
-                                               x_translation = X_OFFSET,
-                                               y_translation = -y_diff,
-                                               zoom = ZOOM_AMOUNT,
-                                               fov = FOV_WINDOW)
-            m.add_frame(img_curr, quality=100)  # 存储mjpeg文件
-            # 3 - # 裁切 320*240 220*100 roi=(x, y, w, h)
-            img_crop = img_curr.crop(roi=(x_crop, y_crop, w_crop, h_crop), copy=True)
-            m_crop.add_frame(img_crop, quality=100)  # 存储mjpeg文件
-            # 4 - 指示灯变化
-            blue_led.toggle()
+        # 计算图像分割
+        split_out = get_split_out(img_moment)
+
+        if flag_record_image:
+            m_moment.add_frame(img_moment, quality=100)  # 存储moment视频
+            blue_led.toggle()  #  指示灯变化
             print('recording')
-        else:
-            img_curr = sensor.snapshot()  # 获取当前传感器图像
 
-            img_curr = img_curr.lens_corr(2.2, 1)  # 原始值2.3 1
-            img_curr = img_curr.rotation_corr(x_rotation = pitch - pitch_offset_deg,
-                                               y_rotation = 0,
-                                               z_rotation = roll,
-                                               x_translation = X_OFFSET,
-                                               y_translation = -y_diff,
-                                               zoom = ZOOM_AMOUNT,
-                                               fov = FOV_WINDOW)
-
-            m_crop = img_curr.crop(roi=(x_crop, y_crop, w_crop, h_crop))
-
-            # 获取moment 或者 lmgd
-
-            # 0328 实现原始图像的difference 暂时保留
-            # m_crop_copy = m_crop.copy()
-            # diff = m_crop.difference(m_crop_last)
-            # m_crop_last = m_crop_copy
-
-            # 0328 尝试计算image moment
-            img_moment = get_moment_image(m_crop)  # 感觉最大可以处理300*100的图像
-
-            # use difference
-            # img_moment_copy = img_moment.copy()
-            # diff = img_moment.difference(m_crop_last)
-            # m_crop_last = img_moment_copy
-
-            # 尝试显示img_moment
-            m_crop.replace(img_moment)
-
-            # 图像分割
-            split_out = get_split_out(img_moment)
-            print(split_out)
     else:
+        # 只存储原始图像，准备废弃
         img_curr = sensor.snapshot()        # 获取图像
         if flag_record_image:
             m_origin.add_frame(img_curr, quality=100)  # 存储原始图像
+        split_out = np.array([0, 0, 0, 0, 0])
 
+    # ----------------------------计算yaw error-----------------------
+    # yaw_error = get_yaw_error(goal_pose, pose_and_yaw)
+    yaw_error = 0
+
+    # ----------------------------计算控制指令--------------------------
+    lgmd_feature = split_out
+    print(lgmd_feature)
+
+    # 需要对lgmd_feature进行归一化，从10-40 归一化到 0-1
+    lgmd_min = 20
+    lgmd_max = 60
+    for i in range(len(lgmd_feature)):
+        lgmd_feature[i] = (lgmd_feature[i] - lgmd_min) / (lgmd_max - lgmd_min)
+        if lgmd_feature[i] > 1:
+            lgmd_feature[i] = 1
+        if lgmd_feature[i] < 0:
+            lgmd_feature[i] = 0
+
+    state_feature = [yaw_error]
+    feature_all = lgmd_feature + state_feature
+
+    feature_input = np.array(feature_all).reshape((6, 1))
+    control_out = np.dot(control_weight, feature_input)[0]
+    if control_out > 1:
+        control_out = 1
+    elif control_out < -1:
+        control_out = -1
+    control_out = 1
+    roll_cmd_deg = control_out * math.radians(40) * 57.3  # 转换成角度，最大30度 这个0.5235是啥没有搞清楚
+
+    send_debug_value(float(roll_cmd_deg))
     # ----------------------------保存录像-----------------------------
     if flag_record_save:
         m_origin.close(fps)
-        m.close(fps)
-        m_crop.close(fps)
+        m_stable.close(fps)
+        m_moment.close(fps)
         flag_record_image = False
         flag_record_save = False
         print('video saved')
 
     # 串口输出
     fps = clock.fps()
-    print('RC: ', rc_in, 'FPS: %.2f' % fps, 'time: ', time.ticks_ms() - now,
+    print('RC: ', rc_in, 'FPS: %.2f' % fps,
+          # 'time: ', time.ticks_ms() - now,
           # ' Free: ' + str(gc.mem_free()), 'step', step,
           'roll: %2.2f' % math.degrees(attitude_rad[0]),
+          'roll_cmd: %2.2f' % roll_cmd_deg,
           'pitch: %2.2f' % math.degrees(attitude_rad[1]),
           'yaw: %.2f' % math.degrees(attitude_rad[2]),
-          'recording: ', flag_record_image
+          # 'recording: ', flag_record_image,
+          'f: ', feature_all
           )
+    # print('input: ', feature_all, 'roll_cmd: ', roll_cmd, 'yaw_error', yaw_error, 'FPS: ', fps, ' Free: ' + str(gc.mem_free()))
     now = time.ticks_ms()
 
 print('finish')
